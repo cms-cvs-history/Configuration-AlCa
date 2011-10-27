bf90563bcdb1aa83e945ba693190f3f66a810fe2
Stephanie Beauceron <sha1-c117a3d4e0f055ce820584107fcb2695f540f4e1@cern.ch>
2011-10-27 15:28:10 +0000

    Update Heavy Ion GT

--- python/autoCond.py
+++ python/autoCond.py
@@ -7,5 +7,5 @@ autoCond = { 'mc'      : 'MC_44_V7::All',
              'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
              'com10'   : 'GR_R_44_V7::All',
              'hltonline' : 'GR_R_44_V7::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI44_V5::All'
+             'starthi' : 'STARTHI44_V6::All'
              }
