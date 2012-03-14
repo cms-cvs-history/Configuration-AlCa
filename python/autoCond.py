6ddb8938718c01e3b7ff4580ecd5b23710380533
cmsbuild <>
2012-03-14 12:44:27 +0000

    This commit was manufactured by cvs2git to create tag 'Configuration- AlCa-V00-00-38'.
    Sprout from master 2011-08-25 09:39:27 UTC Jean-Roch Vlimant <sha1-c20eea45fcd90cd8b9272a4189992d8eff37fabb@cern.ch> 'put back the QCD key'
    Cherrypick from master 2012-03-14 12:44:26 UTC AlCa Service Account <sha1-21daefbb672c4b118ec2e249edee2b53636e70df@cern.ch> 'updated HI':
        python/autoCond.py

--- python/autoCond.py
+++ python/autoCond.py
@@ -1,11 +1,9 @@
 
 
 
-autoCond = { 'mc'      : 'MC_44_V4::All',
-             'startup' : 'START44_V4::All',
-             'craft08' : 'CRFT8_37R_V1::All', # not maintained anymore
-             'craft09' : 'CRFT9_310_V1::All', # not maintained anymore
-             'com10'   : 'GR_R_44_V4::All',
-             'hltonline' : 'GR_R_44_V4::All', # same as com10 until H tag available
-             'starthi' : 'STARTHI44_V4::All'
+autoCond = { 'mc'      : 'MC_50_V16A::All',
+             'startup' : 'START50_V16A::All',
+             'com10'   : 'GR_R_50_V13::All',
+             'hltonline' : 'GR_R_50_V13::All', # same as com10 until H tag available
+             'starthi' : 'STARTHI50_V16::All'
              }
