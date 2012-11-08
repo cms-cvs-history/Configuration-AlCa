dbfd650e8bbaf55e907a903996e74ac0ec115ff8
Martin Grunewald <sha1-9a467096b0966b12a20918c4a8bfa57c962386f8@cern.ch>
2012-11-08 06:00:19 +0000

    move 8e33v1 to v2

--- python/autoCond.py
+++ python/autoCond.py
@@ -103,7 +103,7 @@ autoCond['startup_7E33v4']   = ( autoCond['startup'], ) \
                              + conditions_L1_Run2012C \
                              + conditions_HLT_JECs
 
-autoCond['startup_8E33v1']   = ( autoCond['startup'], ) \
+autoCond['startup_8E33v2']   = ( autoCond['startup'], ) \
                              + conditions_L1_Run2012D
 
 autoCond['startup_GRun']     = ( autoCond['startup'], ) \
@@ -126,7 +126,7 @@ autoCond['hltonline_7E33v3'] = ( autoCond['hltonline'], ) \
 autoCond['hltonline_7E33v4'] = ( autoCond['hltonline'], ) \
                              + conditions_L1_Run2012C
 
-autoCond['hltonline_8E33v1'] = ( autoCond['hltonline'], ) \
+autoCond['hltonline_8E33v2'] = ( autoCond['hltonline'], ) \
                              + conditions_L1_Run2012D
 
 autoCond['hltonline_GRun']   = ( autoCond['hltonline'], ) \
@@ -150,7 +150,7 @@ autoCond['com10_7E33v4']     = ( autoCond['com10'], ) \
                              + conditions_L1_Run2012C \
                              + conditions_HLT_JECs
 
-autoCond['com10_8E33v1']     = ( autoCond['com10'], ) \
+autoCond['com10_8E33v2']     = ( autoCond['com10'], ) \
                              + conditions_L1_Run2012D \
                              + conditions_HLT_JECs
 
